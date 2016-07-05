5b55c02884304ad13fc4666eeb2641362a7e1e8f
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-07-05 12:03:46 +0200

    only tests what needs testing

--- bin/run-phpunit.sh
+++ bin/run-phpunit.sh
@@ -4,16 +4,19 @@ pushd "`dirname $0`/.." > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
-PHPUNIT=${SCRIPTPATH}/lib/vendor/phpunit/phpunit/phpunit
-AUTOLOAD=${SCRIPTPATH}/lib/vendor/autoload.php
+PHPUNIT=${SCRIPTPATH}/vendor/phpunit/phpunit/phpunit
+AUTOLOAD=${SCRIPTPATH}/vendor/autoload.php
 
-PROJECTS="${SCRIPTPATH}/www/sites/all/modules/composer/drupal-sf-dic
-${SCRIPTPATH}/www/sites/all/modules/composer/drupal-ucms
-${SCRIPTPATH}/www/sites/all/modules/composer/drupal-umenu
-${SCRIPTPATH}/lib/vendor/makinacorpus/redis-bundle"
+#PROJECTS="${SCRIPTPATH}/web/sites/all/modules/composer/drupal-sf-dic
+#${SCRIPTPATH}/web/sites/all/modules/composer/drupal-ucms
+#${SCRIPTPATH}/web/sites/all/modules/composer/drupal-umenu
+#${SCRIPTPATH}/vendor/makinacorpus/redis-bundle"
+
+PROJECTS="${SCRIPTPATH}/web/sites/all/modules/composer/drupal-ucms
+${SCRIPTPATH}/web/sites/all/modules/composer/drupal-umenu"
 
 for project in $PROJECTS; do
     cd $project
-    DRUPAL_PATH="${SCRIPTPATH}/www" $PHPUNIT --bootstrap=$AUTOLOAD "$@"
+    DRUPAL_PATH="${SCRIPTPATH}/web" $PHPUNIT --bootstrap=$AUTOLOAD "$@"
     cd - > /dev/null
 done
