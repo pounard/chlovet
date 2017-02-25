0abca8e6c5148691e6dd3467f4cd75f852c27ee6
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-02-25 14:36:00 +0100

    allow dev toolbar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -10,11 +10,19 @@ class AppKernel extends Kernel
      */
     public function registerBundles()
     {
-        return [
+        $bundles = [
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
         ];
+
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new \Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        }
+
+        return $bundles;
     }
 
     /**
@@ -22,23 +30,20 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        if ($this->isFullStack) {
-
-            // Reproduce the config_ENV.yml file from Symfony, but keep it
-            // optional instead of forcing its usage
-            $customConfigFile = $this->rootDir.'/config/config_'.$this->getEnvironment().'.yml';
-            if (!file_exists($customConfigFile)) {
-                // Else attempt with a default one
-                $customConfigFile = $this->rootDir.'/config/config.yml';
-            }
-            if (!file_exists($customConfigFile)) {
-                // If no file is provided by the user, just use the default one
-                // that provide sensible defaults for everything to work fine
-                $customConfigFile = __DIR__.'/../Resources/config/config.yml';
-            }
-
-            $loader->load($customConfigFile);
+        // Reproduce the config_ENV.yml file from Symfony, but keep it
+        // optional instead of forcing its usage
+        $customConfigFile = $this->rootDir.'/config/config_'.$this->getEnvironment().'.yml';
+        if (!file_exists($customConfigFile)) {
+            // Else attempt with a default one
+            $customConfigFile = $this->rootDir.'/config/config.yml';
         }
+        if (!file_exists($customConfigFile)) {
+            // If no file is provided by the user, just use the default one
+            // that provide sensible defaults for everything to work fine
+            $customConfigFile = __DIR__.'/../Resources/config/config.yml';
+        }
+
+        $loader->load($customConfigFile);
     }
 
     /**
