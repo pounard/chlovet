f92865cbc0735348aa8e1a027694d509264d029f
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-12-02 13:37:52 +0100

    patch drupal core to allow drupal.js to work with webpack and global loaders

--- web/misc/drupal.js
+++ web/misc/drupal.js
@@ -1,5 +1,5 @@
 
-var Drupal = Drupal || { 'settings': {}, 'behaviors': {}, 'locale': {} };
+var Drupal = window.Drupal || { 'settings': {}, 'behaviors': {}, 'locale': {} };
 
 // Allow other JavaScript libraries to use $.
 jQuery.noConflict();
